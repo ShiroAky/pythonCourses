6a7a4155b1771eccefe82d5e222f3a8d347497c8
ShiroAky <martinezfranklinjunior7@gmail.com>
2022-07-07 18:03:35 -0400

    Curso 3

---
+++ main.py
@@ -0,0 +1,12 @@
+# Modules:
+from flask import Flask, render_template
+
+# Create a Flask application:
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('localhost', 3000, debug=True)
