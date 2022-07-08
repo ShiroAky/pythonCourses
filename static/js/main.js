28d3971cd0c3ff64848588031e356e331c88d2d9
ShiroAky <martinezfranklinjunior7@gmail.com>
2022-07-07 20:14:50 -0400

    Curso 3

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,8 @@
-alert('Listo')
+// Elements:
+let loader = document.getElementById('loader')
+
+// Functions:
+function Loader() { loader.style.display = 'none' }
+
+// Events:
+window.addEventListener('load', Loader)
